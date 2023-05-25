de13de3f4aab6cd64102e0a3c731eb311489d5c6
Dima <kuzmichman92@gmail.com>
2023-05-25 13:05:06 +0300

    validation_count_consultations_categories_added

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   
   validates :username, uniqueness: true
 
-  # validate :maximum_categories_count
+  validate :maximum_categories_count
 
-  # validate :maximum_consultations_count
+  validate :maximum_consultations_count
 
   has_and_belongs_to_many :categories
 
@@ -22,17 +22,17 @@ class User < ApplicationRecord
 
   ROLES = %i[admin doctor patient]
 
-  # def maximum_consultations_count
-  #   if self.consultations.where(open: true).count > 10
-  #     self.errors.add(:groups, "cannot have more than 10 open")
-  #   end
-  # end
+  def maximum_consultations_count
+    if self.consultations.where(open: true).count > 10
+      self.errors.add(:groups, "cannot have more than 10 open")
+    end
+  end
 
-  # def maximum_categories_count
-  #   if self.categories.count > 1
-  #     self.errors.add(:groups, "cannot have more than 1 category")
-  #   end
-  # end
+  def maximum_categories_count
+    if self.categories.count > 1
+      self.errors.add(:groups, "cannot have more than 1 category")
+    end
+  end
 
   def is?(requested_role)
     self.role == requested_role.to_s
